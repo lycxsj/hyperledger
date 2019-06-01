d42a976048f613b7a2b650f1871d10fc14f23887
Nero1930 <nero1930@163.com>
2019-06-01 08:33:15 +0800

    init router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Body from './Body.jsx';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { Menu, Icon } from 'antd';
+import logo from './logo.svg';
+const { SubMenu }  = Menu;
 
+class App extends React.Component {
+  state = {
+    current: 'Tech'
+  };
+  handleClick = e => {
+    this.setState({
+      current: e.key
+    })
+  };
+  render() {
+    return (
+      <div>
+      <div style={{ display: 'flex', backgroundColor: '#001529' }}>
+      <img src={logo} alt="logo" width="100px" height="48px" /><span style={{color: '#fff', fontSize: '28px'}}>智能服务交易与监管技术平台</span>
+      <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal" theme="dark">
+        <Menu.Item key="Home">
+          <Icon type="home" />
+          首页
+        </Menu.Item>
+        <Menu.Item key="Solution">
+          <Icon type="bulb" />
+          方案
+        </Menu.Item>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="cloud-server" />
+              开放平台
+              <Icon type="caret-down" />
+            </span>
+          }
+        >
+          <Menu.ItemGroup>
+            <Menu.Item key="Baas">BaaS开放平台</Menu.Item>
+            <Menu.Item key="Document">开发文档</Menu.Item>
+            <Menu.Item key="Manual">用户手册</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+        <Menu.Item key="Tech">
+          {/* <a href="https://ant.design" target="_blank" rel="noopener noreferrer"> */}
+          <Icon type="rocket" />
+            技术
+          {/* </a> */}
+        </Menu.Item>
+        <SubMenu
+          title={
+            <span className="submenu-title-wrapper">
+              <Icon type="switcher" />
+              案例
+              <Icon type="caret-down" />
+            </span>
+          }
+        >
+          <Menu.ItemGroup>
+            <Menu.Item key="setting:4">主客体交易</Menu.Item>
+            <Menu.Item key="setting:5">知识产权交易</Menu.Item>
+            <Menu.Item key="setting:6">供应链金融与溯源</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+      </Menu>
+    </div>
+    <Body name={this.state.current} />
+    </div>
+    );
+  }
+}
 export default App;
